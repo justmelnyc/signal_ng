e3a7e8c63dd98fab372dfd17ba96c59508f0cb4a
Ivan Modric <luckysevenbear@gmail.com>
2018-02-05 11:21:33 +0100

    add spots list feature

--- src/app/pages/spots/spots/spots.component.ts
+++ src/app/pages/spots/spots/spots.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { ISpot } from '../../../_core/interfaces/spot';
+
 @Component({
   selector: 'app-spots',
   templateUrl: './spots.component.html',
@@ -10,21 +13,41 @@ export class SpotsComponent implements OnInit, OnDestroy {
 
   private userId: string;
   private sub: any;
+  spots_list: Array<ISpot> = [];
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private adb: AngularFireDatabase
   ) { }
 
   ngOnInit() {
     this.sub = this.activatedRoute.params.subscribe(params => {
       this.userId = params['id'];
-      console.log('======', this.userId);
+
+      if (this.userId) {
+        this.getSpotList();
+      }
     });
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  async getSpotList() {
+    this.adb.list(`spots`, ref => ref.orderByChild('uid').equalTo(this.userId))
+      .snapshotChanges()
+      .subscribe(spots => {
+        spots.map(spot => {
+          this.spots_list.push({
+            uid: spot.key,
+            name: spot.payload.val().name,
+            order: spot.payload.val().order,
+            videos: spot.payload.val().videos
+          });
+        });
+      });
   }
 
   addNewSpot() {
